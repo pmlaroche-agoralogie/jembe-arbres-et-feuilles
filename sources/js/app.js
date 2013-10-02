75ccbc30cb7e38cebc824f9ede7c60befbef648b
pmlaroche-agoralogie <philippe.laroche@agoralogie.fr>
2013-10-02 16:38:20 +0200

    alert test

--- sources/js/app.js
+++ sources/js/app.js
@@ -6,4 +6,26 @@ $(document).ready(function () {
 function test_ecriture() {
          document.getElementById("mytext").innerHTML = "Ceci est un test";
     }
-    
+
+function try_geoloc() {
+	
+	 jembe.geoloc.getCurrentPosition({
+            onSuccess: function(position) {
+ 
+                infoposLat = position.coords.latitude;
+            
+                infoposLong = position.coords.longitude;
+                /*jembe.alert.show({
+                            message:'pos: ' + infoposLat + ", " + infoposLong,
+                            buttons:'Oui',
+                            });
+		*/
+                document.getElementById('geoloc_msg').innerHTML='pos: ' + infoposLat + ", " + infoposLong;
+             }, onError: function (error) { 
+                document.getElementById('geoloc_msg').innerHTML=error.message;
+                //if (error.code==2) alert('Votre position ne peut etre determinee');
+            }
+        });
+        
+	
+}
